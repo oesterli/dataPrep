0c0ce0cee9dfcf167f3907e0ba03c0731089de3e
Amrein Anja <anja.amrein@swisstopo.ch>
2022-03-23 13:34:22 +0100

    harmosBh.py update: explanatory notes for version 1.0

--- scr/harmosBh.py
+++ scr/harmosBh.py
@@ -1,11 +1,12 @@
-# Harmonization of bh data
+# HARMONIZATION of bhData
 
+
+#################################
+## Imports
+#################################
 import datetime
 import glob
 import json
-#################################
-## IMPORTS
-#################################
 import os
 
 import pandas as pd
@@ -23,7 +24,7 @@ ext2 = ".csv"
 enc = conf["file_encoding"]
 nowExport = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-## Read File and create DataFrame based on input data
+## Read input file with layer descriptions and create DataFrame based on input data
 ################################
 multiData = pd.DataFrame()
 files = glob.glob(os.path.join(sourceData, ext1))
@@ -33,23 +34,31 @@ for f in files:
 #     multiData = multiData.append(df, ignore_index=True, sort='false')
 # print(files, multiData)
 
-## simple harmonization function
+## Harmonization function
 ################################
-## Searchlist
+## Open excel with search list with lithological expressions to harmonize
 searchList = glob.glob(os.path.join(sourceSearch, ext1))
 for s in searchList:
     sf = pd.read_excel(s)
 
+## Preparation of expressions to harmonize in search list
 searchList = pd.concat([sf['SEARCH_DE'], sf['SEARCH_FR'], sf['SEARCH_IT']], axis=0, ignore_index=False)
 searchList = searchList.dropna()                          # delete empty cells
 searchList = searchList.reset_index(drop=True)            # reset index from 0 .. i
 # searchList = searchList.astype(str)
 print(searchList)
 
-## Create new column with empty list
+## Preparation of output expressions
+resultList = pd.concat([sf['FIND_DE'], sf['FIND_FR'], sf['FIND_IT']], axis=0, ignore_index=False)
+resultList = resultList.dropna()
+resultList = resultList.reset_index(drop=True)
+print(resultList)
+
+## Create a new column 'myLitho' with empty list for harmonized data
 df['myLitho'] = [[] for _ in range(df.shape[0])]
 # print(searchList[0])
 
+## Looks for expressions of searchList, creates list with indexes refered to and adds harmonized value to column 'myLitho' of the layer
 for i in range(len(searchList)):
     print('----', i)
     ## write indices of selected rows into list
@@ -58,9 +67,12 @@ for i in range(len(searchList)):
 
     ## Loop over list of indices of selected rows
     for j in idx:
-        df.loc[j, 'myLitho'].append(searchList[i])
+       # df.loc[j, 'myLitho'].append(resultList[i], sep='|')
+       # Fehler Stand 23.3.22 : bei mehreren Treffern, wird erstes Resultat einfach überschrieben, es können noch nicht mehrere harmonisierte Begriffe auf einmal zugewiesen werden via Separator '|'
+        df.loc[j, 'myLitho'].append(resultList[i])
 
-## Printing output
+
+## Printing output dataframe
 print(df)
 
 
